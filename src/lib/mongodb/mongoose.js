80915dda0c96ae476df9eccd7b7085aeaaea53fa
Abdi Bekele <abdibekeleb94@gmail.com>
2025-04-29 00:15:44 +0300

    Add MongoDB integration and save user data from Clerk

---
+++ src/lib/mongodb/mongoose.js
@@ -0,0 +1,25 @@
+// mongoose.js
+import mongoose from 'mongoose';
+
+let initialized = false;
+
+export const connect = async () => {
+  mongoose.set('strictQuery', true);
+
+  if (initialized) {
+    console.log('MongoDB already connected');
+    return;
+  }
+
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      dbName: 'next-estate',
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    initialized = true;
+    console.log('MongoDB connected');
+  } catch (error) {
+    console.error('MongoDB connection error:', error);
+  }
+};
